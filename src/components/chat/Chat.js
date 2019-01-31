9ff2d631637e3eea10b32c76f705ee0ebca40349
mokunshao <1627510957@qq.com>
2019-01-31 23:39:35 +0800

    Chat 组件接收数据

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -4,20 +4,62 @@ import Sidebar from "./sidebar/Sidebar";
 import Roster from "./roster/Roster";
 import Panel from "./panel/Panel";
 import "./chat.scss";
+import {showDialog} from '../chat/dialog/Dialog';
 
 class Chat extends Component {
+  state = {
+    roster: [
+      {
+        jid: "asemoemo#chatdemoui_test1@easemob.com",
+        name: "test1",
+        subscription: "both"
+      },
+      {
+        jid: "asemoemo#chatdemoui_test1@easemob.com",
+        name: "test2",
+        subscription: "both"
+      }
+    ]
+  };
   componentDidMount() {
     let token = getToken();
     if (!token) {
       // 项目写好后开启
       // this.props.history.push("/login");
     }
+    conn.listen({
+      onOpened: () => {
+        conn.getRoster({
+          success: roster => {
+            roster = roster.filter(item => {
+              return item.subscription === "both";
+            });
+            this.setState({
+              roster
+            });
+          }
+        });
+      },
+      onRoster: roster => {
+        roster = roster.filter(item => {
+          return item.subscription === "both";
+        });
+        this.setState({
+          roster
+        });
+      },
+      onPresence: e => {
+        if (e.type === "subscribe") {
+          showDialog({ type: 2, e });
+        }
+      }
+    });
   }
   render() {
     return (
       <div className="chat">
         <Sidebar />
-        <Roster friendName={this.props.match.params.friendName} />
+        <Roster friendName={this.props.match.params.friendName} roster={this.state.roster}/>
         <Panel friendName={this.props.match.params.friendName} />
       </div>
     );
--- src/components/chat/roster/Roster.js
+++ src/components/chat/roster/Roster.js
@@ -4,54 +4,11 @@ import { showDialog } from "../dialog/Dialog";
 import { Link } from "react-router-dom";
 
 class Roster extends Component {
-  state = {
-    roster: [
-      {
-        jid: "asemoemo#chatdemoui_test1@easemob.com",
-        name: "test1",
-        subscription: "both"
-      },
-      {
-        jid: "asemoemo#chatdemoui_test1@easemob.com",
-        name: "test2",
-        subscription: "both"
-      }
-    ]
-  };
-  componentDidMount() {
-    conn.listen({
-      onOpened: () => {
-        conn.getRoster({
-          success: roster => {
-            roster = roster.filter(item => {
-              return item.subscription === "both";
-            });
-            this.setState({
-              roster
-            });
-          }
-        });
-      },
-      onRoster: roster => {
-        roster = roster.filter(item => {
-          return item.subscription === "both";
-        });
-        this.setState({
-          roster
-        });
-      },
-      onPresence: e => {
-        if (e.type === "subscribe") {
-          showDialog({ type: 2, e });
-        }
-      }
-    });
-  }
   render() {
     return (
       <section className="roster">
-        {this.state.roster.length
-          ? this.state.roster.map(item => {
+        {this.props.roster.length
+          ? this.props.roster.map(item => {
               let url = `/chat/${item.name}`;
               let isSelected = item.name === this.props.friendName;
               return (
