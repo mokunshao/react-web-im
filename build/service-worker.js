155e0775d9bc0fd09c63948810266d36c5a9b3b5
mokunshao <1627510957@qq.com>
2019-02-04 22:28:25 +0800

    更改路径

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1cebf49d0ba69ac44e96bdab94614cb.js"
+  "/mokunshao/react-web-im/build/precache-manifest.00071bceb61ec8d2dc9e2cb583aeed50.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mokunshao/react-web-im/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
